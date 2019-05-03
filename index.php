89114e086fe6eab46fe55f98e5acbd649b926365
Robert Hill <rhill34@mail.greenriver.edu>
2019-05-03 03:06:41 -0700

    Form2 sticky checkboxes
    Tested, functional

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 /**
  *
  * Title:GRC/IT328/Dating App/index.php
- * Author: Robert Hill III
- * Date: 04.18.2019
+ * Author: Carmen Gallardo && Robert Hill III
+ * Date: 05.02.2019
  * Code Version: V1.0
- * Availability: http://rhill.greenriverdev.com/328/datingB/index.php
+ * Availability: http://rhill.greenriverdev.com/328/pet4/index.php
  *
  */
 
@@ -20,13 +20,23 @@ error_reporting(E_ALL);
 require_once('vendor/autoload.php');
 
 //Require the business logic
-require_once ('model/validation-functions.php');
+require_once('model/validation-functions.php');
 ////Include static head
 //include('views/head.html');
 
 //Create an instance of the Base class
 $f3 = Base::instance();
-$f3->set('colors', array('pink', 'green', 'blue'));
+$f3->set('colors', array(
+    'pink',
+    'green',
+    'blue'
+));
+$f3->set('activity', array(
+    'jump',
+    'kick',
+    'run',
+    'kiss'
+));
 
 //Turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
@@ -35,43 +45,40 @@ $f3->set('DEBUG', 3);
 $f3->route('GET /', function()
 {
     echo "<h1>my Pets</h1><br><p><a href='order'>Order a pet</a></p>";
-//    //Display a view
-//    $view = new Template();
-//    echo $view->render('views/home.html');
+    //    //Display a view
+    //    $view = new Template();
+    //    echo $view->render('views/home.html');
 
 });
 
-$f3->route('GET|POST /order',
-    function($f3){
+$f3->route('GET|POST /order', function($f3)
+{
 
     $animal = null;
-    $qty = null;
+    $qty    = null;
 
-    if(!empty($_POST))
-    {
+    if (!empty($_POST)) {
         $animal = $_POST['animal'];
-        $qty = $_POST['qty'];
+        $qty    = $_POST['qty'];
 
         //Add data to hive
         $f3->set('animal', $animal);
         $f3->set('qty', $qty);
 
-        if(validText($animal) && (validQty($qty)))
-        {
+        if (validText($animal) && (validQty($qty))) {
             $_SESSION['animal'] = $animal;
+            $_SESSION['qty']    = $qty;
             $f3->reroute('/order2');
-        }
+        } //validText($animal) && (validQty($qty))
 
-        if (!validText($animal))
-        {
+        if (!validText($animal)) {
             $f3->set("errors['animal']", "Please enter a valid animal.");
-        }
+        } //!validText($animal)
 
-        if(!validQty($qty))
-        {
+        if (!validQty($qty)) {
             $f3->set("errors['qty']", "Please enter a valid quantity");
-        }
-    }
+        } //!validQty($qty)
+    } //!empty($_POST)
 
     //Display a view
     $view = new Template();
@@ -79,27 +86,32 @@ $f3->route('GET|POST /order',
 
 });
 
-$f3->route('GET|POST /order2',
-      function($f3){
-        if(isset($_POST['color'])) {
-            $color = $_POST['color'];
-            if(validColor($color)){
-                $_SESSION['color'] = $color;
-                $f3->reroute('/results');
-            }else {
-                $f3->set("errors['color']", "Please enter a color.");
-            }
+$f3->route('GET|POST /order2', function($f3)
+{
+    if (!empty($_POST)) {
+        $color = $_POST['color'];
+        $act   = $_POST['act'];
+
+        if (validColor($color) && validCheck($act)) {
+            $_SESSION['color'] = $color;
+            $_SESSION['act']   = $act;
+
+            $f3->reroute('/results');
+        } //validColor($color) && validCheck($act)
+        else {
+            $f3->set("errors['color']", "Please enter a color.");
+            $f3->set("errors['act']", "Your pet cannot do that right now! Choose an Activity");
         }
-          //Display a view
-          $view = new Template();
-          echo $view->render('views/form2.html');
+    } //!empty($_POST)
+    //Display a view
+    $view = new Template();
+    echo $view->render('views/form2.html');
 });
 //Define a Lunch route with a parameter
-$f3->route('GET /@animal', function($f3,$params)
+$f3->route('GET /@animal', function($f3, $params)
 {
     $animal = $params['animal'];
-    switch($animal)
-    {
+    switch ($animal) {
         case 'chicken':
             echo "Cluck!";
             break;
@@ -122,18 +134,20 @@ $f3->route('GET /@animal', function($f3,$params)
 
         default:
             $f3->error(404);
-    }
+    } //$animal
 });
 
 $f3->route('GET|POST /results', function()
 {
-//    $_SESSION['color'] = $_POST['color'];
-//    print_r($_SESSION);
+    //    $_SESSION['color'] = $_POST['color'];
+    //    print_r($_SESSION);
+    $interest              = implode(", ", $_SESSION['act']);
+    $_SESSION['actString'] = $interest;
     //Display a view
-    $view = new Template();
+    $view                  = new Template();
     echo $view->render('views/results.html');
 
 });
 
 //Run fat free
-$f3->run();
+$f3->run(); 
--- model/validation-functions.php
+++ model/validation-functions.php
@@ -7,7 +7,7 @@ function validColor($color)
     return in_array($color, $f3->get('colors'));
 }
 
-function validText( $string )
+function validText($string)
 {
     return (!empty($string) && ctype_alpha($string));
 }
@@ -15,3 +15,8 @@ function validQty($qty)
 {
     return (!empty($qty) && is_numeric($qty) && $qty >= 1);
 }
+function validCheck($act)
+{
+    global $f3;
+    return (!empty($act) && 0 == count(array_diff($act, $f3->get('activity'))));
+} 
